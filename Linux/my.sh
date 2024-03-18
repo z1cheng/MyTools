dbb047b09266bd8dfd9d6aa7f3d6d6cfc4423b90
z1cheng <imchench@gmail.com>
2024-03-18 23:48:49 +0800

    Add etcdctl alias
    Signed-off-by: z1cheng <imchench@gmail.com>

--- Linux/my.sh
+++ Linux/my.sh
@@ -8,3 +8,7 @@ bash -c "for b in {0..99999999}; do a=$b$a; done"
 # tee -a: Append the output to the files rather than overwriting them.
 echo "your text" | sudo tee -a /path/to/your/file
 sudo bash -c "echo 'your text' >> /path/to/your/file"
+
+# Etcdctl
+etcdPodName="etcd-minikube"
+alias ectl="kubectl exec -it -n kube-system ${etcdPodName} -- etcdctl --cacert='/var/lib/minikube/certs/etcd/ca.crt' --cert='/var/lib/minikube/certs/etcd/peer.crt' --key='/var/lib/minikube/certs/etcd/peer.key'"
